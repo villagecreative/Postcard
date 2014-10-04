50caccc9ffa80af4244437c0494fb56db0d4add3
David <mac@Davids-MacBook-Pro.local>
2014-10-04 14:25:41 -0700

    Challenge 1 - name label functionality

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var messageLabel: UILabel!
     @IBOutlet weak var enterNameTextField: UITextField!
     @IBOutlet weak var enterMessageTextField: UITextField!
@@ -27,10 +28,17 @@ class ViewController: UIViewController {
 
     @IBAction func sendMailButtonPressed(sender: AnyObject) {
         // Code will evaluate when we press the button
-        // Adding a comment here to test commits
+        
+        nameLabel.hidden = false
+        nameLabel.text = enterNameTextField.text
+        nameLabel.textColor = UIColor.blueColor()
+        
         messageLabel.hidden = false
         messageLabel.text = enterMessageTextField.text
         messageLabel.textColor = UIColor.redColor()
+        
+        enterNameTextField.text = ""
+        enterNameTextField.resignFirstResponder()
         
         enterMessageTextField.text = ""
         enterMessageTextField.resignFirstResponder()
